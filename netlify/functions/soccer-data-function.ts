6e1f0ac5d3f822e232c24521467b1c75f79f2688
Holger Grosse-Plankermann <holgergp@gmail.com>
2022-08-17 15:36:35 +0200

    Add netlify data functions

---
+++ netlify/functions/soccer-data-function.ts
@@ -0,0 +1,15 @@
+import { Handler } from "@netlify/functions";
+import { SAMPLE_LEAGUE_TABLE } from "../../src/components/LeagueTable/SampleData";
+
+const handler: Handler = async (event) => {
+  if (event.httpMethod === "GET") {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(SAMPLE_LEAGUE_TABLE),
+    };
+  } else {
+    return { statusCode: 405 };
+  }
+};
+
+export { handler };
